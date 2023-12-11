ba806579c66f3c6bc431c60009e7adc82005b91a
Tunghq <tunghqor@gmail.com>
2023-12-12 02:27:19 +0700

    Add all

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,11 @@
   const FRAME_RATE = 60
   const PARTICLE_NUM = 2000
   const RADIUS = Math.PI * 2
-  const CANVASWIDTH = 500
+  const CANVASWIDTH = 2000
   const CANVASHEIGHT = 150
   const CANVASID = 'canvas'
 
-  let texts = ['MY DEAR', 'LOOK UP AT THE', 'STARRY SKY', 'ARE YOU', 'LOOKING AT THE', 'SAME STAR', 'WITH ME ?', 'HAPPY', 'CHINESE', 'VALENTINE\'S', 'DAY', 'I MISS YOU']
+  let texts = ['KIỀU OANH', 'ANH MUỐN MANG EM ĐI', 'RỜI XA ', 'THỨ KHÔNG LÀM EM VUI', 'BẢO BỌC EM.', 'MONG EM', 'VUI VẺ', 'NHƯ NHỮNG GÌ', 'EM BIỂU HIỆN RA.' , 'BẢO BỐI CỦA ANH', 'ANH YÊU EM']
 
   let canvas,
     ctx,
@@ -41,7 +41,7 @@
   }
 
   function particleText (imgData) {
-    // 点坐标获取
+    
     var pxls = []
     for (var w = CANVASWIDTH; w > 0; w -= 3) {
       for (var h = 0; h < CANVASHEIGHT; h += 3) {
@@ -157,23 +157,22 @@
     constructor (canvas) {
       let spread = canvas.height
       let size = Math.random() * 1.2
-      // 速度
+      
       this.delta = 0.06
-      // 现在的位置
+     
       this.x = 0
       this.y = 0
-      // 上次的位置
+      
       this.px = Math.random() * canvas.width
       this.py = (canvas.height * 0.5) + ((Math.random() - 0.5) * spread)
-      // 记录点最初的位置
+      
       this.mx = this.px
       this.my = this.py
-      // 点的大小
+      
       this.size = size
-      // this.origSize = size
-      // 是否用来显示字
+      
       this.inText = false
-      // 透明度相关
+      
       this.opacity = 0
       this.fadeInRate = 0.005
       this.fadeOutRate = 0.03
